7f960e7b6689dda9c2f9537997730ed7df1b4dbb
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2025-06-24 12:25:27 +0600

    Working in invoice

--- app/Http/Controllers/Clients/UserController.php
+++ app/Http/Controllers/Clients/UserController.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 
+
 class UserController extends Controller
 {
     public function index()
